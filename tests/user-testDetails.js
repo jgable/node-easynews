b07c8afb3a517d27a52234191cde7e79d00a3eea
Jacob Gable <jacob.gable@gmail.com>
2012-05-20 09:56:08 -0500

    Removing user-testDetails.js; cannot add to gitignore while tracked

--- tests/user-testDetails.js
+++
@@ -1,8 +0,0 @@
-
-// These should be overridden by individual developers when testing.  
-// Try not to check in your user and pass when contributing.
-
-module.exports = {
-  user: "<REPLACE_ME>",
-  pass: "<REPLACE_ME>"
-};
